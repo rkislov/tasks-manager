9affdb09a0f4dc6f90564a35ec99a24b14da0253
Roman Kislov <roman@kislovs.ru>
2019-04-17 17:58:55 +0500

    validation sanitization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser:true,
@@ -7,36 +8,70 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User',{
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim:true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if(value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required:true,
+        trim:true,
+        minlength:7,
+        validate(v){
+            if(v.toLowerCase().includes('password')) {
+                throw new Error('Password contain password')
+            }
+        }
     }
 })
 
 // const me = new User({
-//     name: "Roman",
-//     age: 'Mike'
+//     name: "   Roman",
+//     email: 'ROMAN@kislovs.ru   ',
+//     password: 'passWOrdtttt  '
 // })
 //
 // me.save().then(()=>{
 //     console.log(me)
 // }).catch((error)=>{
-//     console.log('Error! ',error)
+//     console.log('Error! ',error.message)
 // })
 
 const Task = mongoose.model('Task',{
     description: {
-        type: String
+        type: String,
+        trim: true,
+        required:true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
 const task = new Task({
-    description: "first task",
-    completed: true
+    description: "   first task1",
+
 })
 
 task.save().then(()=>{
