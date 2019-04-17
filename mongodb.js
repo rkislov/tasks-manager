8161025f2d8ffe564348a473855a83c8deb61953
Roman Kislov <roman@kislovs.ru>
2019-04-17 14:23:00 +0500

    First commit

---
+++ mongodb.js
@@ -0,0 +1,19 @@
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+
+
+const {MongoClient, ObjectID } = require('mongodb')
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+
+MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
+    if (error) {
+        return console.log('unavle to connect to database!')
+    }
+
+    const db = client.db(databaseName)
+
+})
