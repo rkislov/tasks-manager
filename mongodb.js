bc0893b12da2e73e8cf81c935e605aed2bdf9625
Roman Kislov <roman@kislovs.ru>
2019-04-17 16:33:34 +0500

    Add Find

--- mongodb.js
+++ mongodb.js
@@ -15,5 +15,36 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
     }
 
     const db = client.db(databaseName)
+    //
+    // db.collection('users').findOne({_id: new ObjectID("5cb5d8e55cd46f7f67696c5d")},(error,user) => {
+    //     if (error) {
+    //         return console.log('Unable to fetch')
+    //     }
+    //
+    //     console.log(user)
+    // })
+    //
+    // db.collection('users').find({}).toArray((error, users)=> {
+    //     if (error) {
+    //         return console.log('Unnable to find users')
+    //     }
+    //
+    //     console.log(users)
+    // })
 
+    db.collection('tasks').findOne({_id:new ObjectID("5cb5de5860205c057aaff745")}, (error,task)=> {
+        if (error){
+            return console.log('Unable to find task')
+        }
+
+        console.log(task)
+    })
+
+    db.collection('tasks').find({completed:false}).toArray((error,tasks) => {
+        if (error) {
+            return console.log('Unable to find tasks')
+        }
+
+        console.log(tasks)
+    })
 })
